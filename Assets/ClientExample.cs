71eab66906584d1317418f98d38637f4aa472e94
unknown <charokoukuu@gmail.com>
2020-02-21 21:47:57 +0900

    aaa

--- Assets/ClientExample.cs
+++ Assets/ClientExample.cs
@@ -14,7 +14,7 @@ public class ClientExample : MonoBehaviour
         int i = 0;
     void Start()
     {
-        ws = new WebSocket("ws://192.168.0.10:3000/");
+        ws = new WebSocket("ws://192.168.0.15:3000/");
 
         ws.OnOpen += (sender, e) =>
         {
@@ -45,7 +45,8 @@ public class ClientExample : MonoBehaviour
         X.GetComponent<Text>().text = "X: " + Input.acceleration.x.ToString("F2");
         Y.GetComponent<Text>().text = "Y: " + Input.acceleration.y.ToString("F2");
         Z.GetComponent<Text>().text = "Z: " + Input.acceleration.z.ToString("F2");
-        ws.Send(Input.acceleration.z.ToString("F2"));
+        ws.Send(Input.acceleration.x.ToString("F2")+","+ Input.acceleration.y.ToString("F2"));
+
 
     }
 
